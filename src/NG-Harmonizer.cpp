7cc26ce8af82b2495721e43baeaa7b5fca2aa5ea
Nicholas Gilbert <nickgilbert2@gmail.com>
2019-05-03 16:48:28 -0700

    Adds some experimental stuff, doesn't actually do harmonies yet

--- src/NG-Harmonizer.cpp
+++ src/NG-Harmonizer.cpp
@@ -1,5 +1,6 @@
 #include "toot-toot-modules.hpp"
-
+#include "dsp/digital.hpp"
+#include "dsp/ringbuffer.hpp"
 
 struct NGHarmonizer : Module {
 	enum ParamIds {
@@ -8,10 +9,12 @@ struct NGHarmonizer : Module {
 	};
 	enum InputIds {
 		WAVE_INPUT,
+		PITCH_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
 		OUTPUT_LOW,
+		OUTPUT_THRU,
 		OUTPUT_HIGH,
 		NUM_OUTPUTS
 	};
@@ -20,8 +23,16 @@ struct NGHarmonizer : Module {
 		NUM_LIGHTS
 	};
 
-	float phase = 0.0;
-	float blinkPhase = 0.0;
+	float upper = 10.0f;
+	float lower = -10.0f;
+	
+	float FREQ_C4 = 261.6256f;
+
+	int delay_lim = 100;
+	int delay_counter = 0;
+	RingBuffer<float, 500> upper_ring;
+	RingBuffer<float, 500> lower_ring;
+	SchmittTrigger trigger;
 
 	NGHarmonizer() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
 	void step() override;
@@ -35,12 +46,26 @@ struct NGHarmonizer : Module {
 
 void NGHarmonizer::step() {
 	float pitch = params[PITCH_PARAM].value;
+	if (inputs[PITCH_INPUT].active == true) {
+		pitch = inputs[PITCH_INPUT].value;
+	}
 	float input_wave = inputs[WAVE_INPUT].value;
+	float freq = FREQ_C4 * pow(2, input_wave);
+	
+	//printf("Freq: %f\n", freq);
 
-	outputs[OUTPUT_LOW].value = input_wave - pitch;
-	outputs[OUTPUT_HIGH].value = input_wave + pitch;
+
+	float freq_low = freq - (pitch * 100);
+	float freq_high = freq + (pitch * 100); 
+
+	float output_low = clamp(log2f(freq_low / FREQ_C4), lower, upper);
+	float output_high = clamp(log2f(freq_high / FREQ_C4), lower, upper);
+
+	outputs[OUTPUT_LOW].value = output_low;
+	
+	outputs[OUTPUT_THRU].value = input_wave;
+	outputs[OUTPUT_HIGH].value = output_high;
 }
-
 
 struct NGHarmonizerWidget : ModuleWidget {
 	NGHarmonizerWidget(NGHarmonizer *module) : ModuleWidget(module) {
@@ -51,12 +76,14 @@ struct NGHarmonizerWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, NGHarmonizer::PITCH_PARAM, -3.0, 3.0, 0.0));
+		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, NGHarmonizer::PITCH_PARAM, -10.0, 10.0, 0.0));
+		addInput(Port::create<PJ301MPort>(Vec(33, 150), Port::INPUT, module, NGHarmonizer::PITCH_INPUT));
 
 		addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, NGHarmonizer::WAVE_INPUT));
 
-		addOutput(Port::create<PJ301MPort>(Vec(18, 275), Port::OUTPUT, module, NGHarmonizer::OUTPUT_LOW));
-		addOutput(Port::create<PJ301MPort>(Vec(48, 275), Port::OUTPUT, module, NGHarmonizer::OUTPUT_HIGH));
+		addOutput(Port::create<PJ301MPort>(Vec(5, 255), Port::OUTPUT, module, NGHarmonizer::OUTPUT_LOW));
+		addOutput(Port::create<PJ301MPort>(Vec(33, 255), Port::OUTPUT, module, NGHarmonizer::OUTPUT_THRU));
+		addOutput(Port::create<PJ301MPort>(Vec(61, 255), Port::OUTPUT, module, NGHarmonizer::OUTPUT_HIGH));
 
 		addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, NGHarmonizer::BLINK_LIGHT));
 	}
