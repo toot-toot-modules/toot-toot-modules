ac2b6655795601f2cbc93d055510e48857506a6d
Nicholas Gilbert <nickgilbert2@gmail.com>
2019-05-02 17:18:32 -0700

    NG-Harmonizer: Adds basic wave passthrough, with pitch knob
    to add and subtract from the high and low outputs

--- src/NG-Harmonizer.cpp
+++ src/NG-Harmonizer.cpp
@@ -7,11 +7,12 @@ struct NGHarmonizer : Module {
 		NUM_PARAMS
 	};
 	enum InputIds {
-		PITCH_INPUT,
+		WAVE_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
-		SINE_OUTPUT,
+		OUTPUT_LOW,
+		OUTPUT_HIGH,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
@@ -33,30 +34,11 @@ struct NGHarmonizer : Module {
 
 
 void NGHarmonizer::step() {
-	// Implement a simple sine oscillator
-	float deltaTime = engineGetSampleTime();
-
-	// Compute the frequency from the pitch parameter and input
 	float pitch = params[PITCH_PARAM].value;
-	pitch += inputs[PITCH_INPUT].value;
-	pitch = clamp(pitch, -4.0f, 4.0f);
-	// The default pitch is C4
-	float freq = 261.626f * powf(2.0f, pitch);
+	float input_wave = inputs[WAVE_INPUT].value;
 
-	// Accumulate the phase
-	phase += freq * deltaTime;
-	if (phase >= 1.0f)
-		phase -= 1.0f;
-
-	// Compute the sine output
-	float sine = sinf(2.0f * M_PI * phase);
-	outputs[SINE_OUTPUT].value = 5.0f * sine;
-
-	// Blink light at 1Hz
-	blinkPhase += deltaTime;
-	if (blinkPhase >= 1.0f)
-		blinkPhase -= 1.0f;
-	lights[BLINK_LIGHT].value = (blinkPhase < 0.5f) ? 1.0f : 0.0f;
+	outputs[OUTPUT_LOW].value = input_wave - pitch;
+	outputs[OUTPUT_HIGH].value = input_wave + pitch;
 }
 
 
@@ -71,9 +53,10 @@ struct NGHarmonizerWidget : ModuleWidget {
 
 		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, NGHarmonizer::PITCH_PARAM, -3.0, 3.0, 0.0));
 
-		addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, NGHarmonizer::PITCH_INPUT));
+		addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, NGHarmonizer::WAVE_INPUT));
 
-		addOutput(Port::create<PJ301MPort>(Vec(33, 275), Port::OUTPUT, module, NGHarmonizer::SINE_OUTPUT));
+		addOutput(Port::create<PJ301MPort>(Vec(18, 275), Port::OUTPUT, module, NGHarmonizer::OUTPUT_LOW));
+		addOutput(Port::create<PJ301MPort>(Vec(48, 275), Port::OUTPUT, module, NGHarmonizer::OUTPUT_HIGH));
 
 		addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, NGHarmonizer::BLINK_LIGHT));
 	}
