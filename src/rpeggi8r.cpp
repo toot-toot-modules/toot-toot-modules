b6bb21a757c835eef22dc723004ab9af7a09826b
Cadence Michael <alysaur@users.noreply.github.com>
2019-05-25 18:54:39 -0700

    Implement basic linear appegiation using knobs

--- src/rpeggi8r.cpp
+++ src/rpeggi8r.cpp
@@ -3,62 +3,83 @@
 
 struct Rpeggi8r : Module {
 	enum ParamIds {
-		PITCH_PARAM,
+		FREQ_PARAM,
+		A_PARAM,
+		B_PARAM,
+		C_PARAM,
+		D_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
-		PITCH_INPUT,
+		//FREQ_INPUT,
+		//A_INPUT,
+		//B_INPUT,
+		//C_INPUT,
+		//D_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
-		SINE_OUTPUT,
+		ARP_OUTPUT,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
-		BLINK_LIGHT,
+		//BLINK_LIGHT,
 		NUM_LIGHTS
 	};
 
 	float phase = 0.0;
 	float blinkPhase = 0.0;
+	float arpPhase = 0.0;
+	int currentTone;
 
-	Rpeggi8r() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	Rpeggi8r();
 	void step() override;
 
-	// For more advanced Module features, read Rack's engine.hpp header file
-	// - toJson, fromJson: serialization of internal data
-	// - onSampleRateChange: event triggered by a change of sample rate
-	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
+	void linearAppegiate();
 };
 
 
+Rpeggi8r::Rpeggi8r() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+	currentTone = A_PARAM;
+}
+
 void Rpeggi8r::step() {
-	// Implement a simple sine oscillator
+	// Get the sample time for time related calculations
 	float deltaTime = engineGetSampleTime();
 
-	// Compute the frequency from the pitch parameter and input
-	float pitch = params[PITCH_PARAM].value;
-	pitch += inputs[PITCH_INPUT].value;
-	pitch = clamp(pitch, -4.0f, 4.0f);
-	// The default pitch is C4
-	float freq = 261.626f * powf(2.0f, pitch);
+	// Get the appregiation frequency
+	float freq = params[FREQ_PARAM].value;
 
-	// Accumulate the phase
-	phase += freq * deltaTime;
+	// Appegiate when enough time has passed
+	arpPhase += freq * deltaTime;
+	if (arpPhase >= 1.0f) {
+		arpPhase -= 1.0f;
+		linearAppegiate();
+	}
+
+	// Get the output frequency for the current tone
+	float tone = params[currentTone].value;
+
+	// Accumulate phase for the output sine wave
+	phase += tone * deltaTime;
 	if (phase >= 1.0f)
 		phase -= 1.0f;
 
-	// Compute the sine output
+	// Calculate the value of the current tone
 	float sine = sinf(2.0f * M_PI * phase);
-	outputs[SINE_OUTPUT].value = 5.0f * sine;
 
-	// Blink light at 1Hz
-	blinkPhase += deltaTime;
-	if (blinkPhase >= 1.0f)
-		blinkPhase -= 1.0f;
-	lights[BLINK_LIGHT].value = (blinkPhase < 0.5f) ? 1.0f : 0.0f;
+	// Send tone to arpeggiation output
+	outputs[ARP_OUTPUT].value = 5.0f * sine;
 }
 
+void Rpeggi8r::linearAppegiate() {
+	// Move to next tone
+	currentTone++;
+
+	// Reset back to first tone after moving past the last tone
+	if (currentTone == NUM_PARAMS)
+		currentTone = A_PARAM;
+}
 
 struct Rpeggi8rWidget : ModuleWidget {
 	Rpeggi8rWidget(Rpeggi8r *module) : ModuleWidget(module) {
@@ -69,19 +90,23 @@ struct Rpeggi8rWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, Rpeggi8r::PITCH_PARAM, -3.0, 3.0, 0.0));
+		addParam(ParamWidget::create<RoundLargeBlackKnob>(Vec(30, 60), module, Rpeggi8r::FREQ_PARAM, 1.0f, 16.0f, 4.0f));
+		addParam(ParamWidget::create<RoundBlackKnob>(Vec(15, 115), module, Rpeggi8r::A_PARAM, 432.0f, 864.0f, 432.0f));
+		addParam(ParamWidget::create<RoundBlackKnob>(Vec(50, 115), module, Rpeggi8r::B_PARAM, 432.0f, 864.0f, 544.29f));
+		addParam(ParamWidget::create<RoundBlackKnob>(Vec(15, 150), module, Rpeggi8r::C_PARAM, 432.0f, 864.0f, 647.27f));
+		addParam(ParamWidget::create<RoundBlackKnob>(Vec(50, 150), module, Rpeggi8r::D_PARAM, 432.0f, 864.0f, 864.0f));
 
-		addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, Rpeggi8r::PITCH_INPUT));
+		//addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, Rpeggi8r::FREQ_INPUT));
+		//addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, Rpeggi8r::A_INPUT));
+		//addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, Rpeggi8r::B_INPUT));
+		//addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, Rpeggi8r::C_INPUT));
+		//addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, Rpeggi8r::D_INPUT));
 
-		addOutput(Port::create<PJ301MPort>(Vec(33, 275), Port::OUTPUT, module, Rpeggi8r::SINE_OUTPUT));
+		addOutput(Port::create<PJ301MPort>(Vec(50, 300), Port::OUTPUT, module, Rpeggi8r::ARP_OUTPUT));
 
-		addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, Rpeggi8r::BLINK_LIGHT));
+		//addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, Rpeggi8r::BLINK_LIGHT));
 	}
 };
 
 
-// Specify the Module and ModuleWidget subclass, human-readable
-// author name for categorization per plugin, module slug (should never
-// change), human-readable module name, and any number of tags
-// (found in `include/tags.hpp`) separated by commas.
-Model *modelRpeggi8r = Model::create<Rpeggi8r, Rpeggi8rWidget>("toot-toot-modules", "Rpeggi8r", "Arpeggiator for different chords", OSCILLATOR_TAG);
+Model *modelRpeggi8r = Model::create<Rpeggi8r, Rpeggi8rWidget>("toot-toot-modules", "Rpeggi8r", "Quad input arpeggiator", OSCILLATOR_TAG);
